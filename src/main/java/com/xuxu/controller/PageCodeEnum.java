2c53edaa7120ba75a925ef09de25788d9d605fc3
xuxu <270692929@qq.com>
2018-03-17 22:42:37 +0800

    添加广告保存后提示信息

---
+++ src/main/java/com/xuxu/controller/PageCodeEnum.java
@@ -0,0 +1,28 @@
+package com.xuxu.controller;
+
+public enum PageCodeEnum {
+	
+	ADD_SUCCESS(1000,"新增成功"),
+	ADD_FAIL(1001,"新增失败"); 
+	
+	private Integer code;
+	private String msg;
+	
+	public static final String KEY = "pageCode";
+	
+	private PageCodeEnum(Integer code,String msg){
+		this.code = code;
+		this.msg  = msg;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+	
+	
+}
+
--- src/main/java/com/xuxu/controller/content/AdController.java
+++ src/main/java/com/xuxu/controller/content/AdController.java
@@ -2,10 +2,11 @@ package com.xuxu.controller.content;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.xuxu.dao.AdDao;
+import com.xuxu.controller.PageCodeEnum;
 import com.xuxu.dto.AdDto;
 import com.xuxu.service.AdService;
 
@@ -26,8 +27,12 @@ public class AdController {
 	}
 	
 	@RequestMapping(value="/add",method=RequestMethod.POST)
-	public String add(AdDto adDto){
-		adService.insert(adDto);
+	public String add(AdDto adDto,Model model){
+		if(adService.insert(adDto)){
+			model.addAttribute(PageCodeEnum.KEY,PageCodeEnum.ADD_SUCCESS);
+		}else{
+			model.addAttribute(PageCodeEnum.KEY,PageCodeEnum.ADD_FAIL);
+		}
 		return "/content/adAdd";
 	}
 }
